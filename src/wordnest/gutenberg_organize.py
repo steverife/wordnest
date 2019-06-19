6a9320fde138d159ee2a70df69d4c60f960b80ff
srife <srife@mit.edu>
2019-06-18 22:10:56 -0400

    regex patterns class

--- src/wordnest/gutenberg_organize.py
+++ src/wordnest/gutenberg_organize.py
@@ -4,19 +4,28 @@ from collections import namedtuple
 
 from requests import get
 
+class Patterns:
+    listing_start = "<==LISTINGS==>"
+    listing_comment = re.compile(r"[\s]?[\*]{4}[\s\S]+[\*]{4}[\s]?")
+    listing_date = re.compile(r"to (?P<date>[0-9]{1,2}) (?P<month>[A-Z]{1}[a-z]{2,3}) (?P<year>[0-9]{4})")
+    listing_date_divider = r"~ ~ ~ ~ Posting Dates for the below eBooks:  "
+    listing_title_line = re.compile(r"([\w]+[\s\S]+)[\s]+([0-9]+)")
+    listing_header_line = re.compile(r"TITLE and AUTHOR[\s\S]+EBOOK NO\.")
+    listing_metadata = re.compile(r" \[((?:).*)\: ((?:).*)\]")
+
+RE_PATTERNS = Patterns()
 YearMonth = namedtuple('year_month', ['year', 'month'])
 
-listing_start_pattern = "<==LISTINGS==>"
-listing_comment_pattern = re.compile(r"[\s]?[\*]{4}[\s\S]+[\*]{4}[\s]?")
-listing_date_pattern = re.compile(r"to (?P<date>[0-9]{1,2}) (?P<month>[A-Z]{1}[a-z]{2,3}) (?P<year>[0-9]{4})")
-listing_date_divider_pattern = r"~ ~ ~ ~ Posting Dates for the below eBooks:  "
 
-listing_title_line_pattern = re.compile(r"([\w]+[\s\S]+)[\s]+([0-9]+)")
-listing_header_line_pattern = re.compile(r"TITLE and AUTHOR[\s\S]+EBOOK NO\.")
-listing_metadata_pattern = re.compile(r" \[((?:).*)\: ((?:).*)\]")
+"""
+  given text of this pattern:
+    eBooks:  1 Jun 2019 to 30 Jun 2019 ~ ~ ~ ~
+  return a YearMonth named tuple
+  return None if not found 
 
+"""
 def extract_year_month(text):
-    match = listing_date_pattern.search(text)
+    match = RE_PATTERNS.listing_date.search(text)
     if match:
         try:
             month_text = match.groupdict().get('month')
@@ -50,10 +59,10 @@ def parse_book_info(text):
     book_id = None
     buffer_lines = []
     for line in text.splitlines():
-        if listing_comment_pattern.match(line) or listing_header_line_pattern.match(line):
+        if RE_PATTERNS.listing_comment.match(line) or RE_PATTERNS.listing_header_line.match(line):
             return None
-        title_line_match = listing_title_line_pattern.match(line)
-        listing_metadata_match = listing_metadata_pattern.match(line)
+        title_line_match = RE_PATTERNS.listing_title_line.match(line)
+        listing_metadata_match = RE_PATTERNS.listing_metadata.match(line)
         if title_line_match:
             book_id = title_line_match.groups()[1].strip()
             buffer_lines.append(title_line_match.groups()[0].strip())
@@ -67,7 +76,7 @@ def parse_book_info(text):
     title = arranged_lines[0]
     if len(arranged_lines) > 1:
         for line in arranged_lines[1:]:
-            listing_metadata_match = listing_metadata_pattern.match(line)
+            listing_metadata_match = RE_PATTERNS.listing_metadata.match(line)
             if listing_metadata_match:
                 [key, value] = listing_metadata_match.groups()[:2]
                 metadata[key.strip().lower()] = value.strip()
@@ -83,25 +92,34 @@ def parse_book_info(text):
     return None
 
 def gutenberg_divide_index_into_tables(text):
-    halves = re.split(listing_start_pattern, text)
+    halves = re.split(RE_PATTERNS.listing_start, text)
     book_list = []
     if len(halves) != 2:
         raise Exception('not', 'divided')
-    for text in re.split(listing_date_divider_pattern, halves[1]):
-        book_list += parse_table(text)
+    for table_text in re.split(RE_PATTERNS.listing_date_divider, halves[1]):
+        book_list += parse_table(table_text)
     return book_list
 
 def gutenberg_text_to_json(text):
     return gutenberg_divide_index_into_tables(text)
 
 def arrange_book_text(text):
-    text = remove_duplicate_spaces(text)
-    text = text.replace('[','\n[')
+    text = remove_extra_whitespace(text)
+    text = text.replace('[', '\n[')
     return text
 
-def remove_duplicate_spaces(text):
+"""
+ remove multiple whitespaces in a row
+ remove leading and trailing whitespace
+"""
+def remove_extra_whitespace(text):
     return re.sub(r'(\s){2,}', ' ', text).strip()
 
+"""
+ normalize the full text of document
+  - normalize line ends
+
+"""
 def normalize_text(text):
     return text.replace("\r\n", "\n")
 
