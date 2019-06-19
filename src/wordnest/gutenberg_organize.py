e48d1bae2c67d594c0968259a45375d34affd3b0
srife <srife@mit.edu>
2019-06-18 21:37:14 -0400

    tidying

--- src/wordnest/gutenberg_organize.py
+++ src/wordnest/gutenberg_organize.py
@@ -1,20 +1,19 @@
-from requests import get  
 import re
 from time import strptime
-
 from collections import namedtuple
 
-year_month = namedtuple('year_month', ['year', 'month'])
+from requests import get
+
+YearMonth = namedtuple('year_month', ['year', 'month'])
 
 listing_start_pattern = "<==LISTINGS==>"
-listing_comment_pattern = re.compile("[\s]?[\*]{4}[\s\S]+[\*]{4}[\s]?")
-listing_date_pattern = re.compile("to (?P<date>[0-9]{1,2}) (?P<month>[A-Z]{1}[a-z]{2,3}) (?P<year>[0-9]{4})")
+listing_comment_pattern = re.compile(r"[\s]?[\*]{4}[\s\S]+[\*]{4}[\s]?")
+listing_date_pattern = re.compile(r"to (?P<date>[0-9]{1,2}) (?P<month>[A-Z]{1}[a-z]{2,3}) (?P<year>[0-9]{4})")
+listing_date_divider_pattern = r"~ ~ ~ ~ Posting Dates for the below eBooks:  "
 
-listing_date_divider_pattern = "~ ~ ~ ~ Posting Dates for the below eBooks:  "
-
-listing_title_line_pattern = re.compile("([\w]+[\s\S]+)[\s]+([0-9]+)")
-listing_header_line_pattern = re.compile("TITLE and AUTHOR[\s\S]+EBOOK NO\.")
-listing_metadata_pattern = re.compile(" \[((?:).*)\: ((?:).*)\]")
+listing_title_line_pattern = re.compile(r"([\w]+[\s\S]+)[\s]+([0-9]+)")
+listing_header_line_pattern = re.compile(r"TITLE and AUTHOR[\s\S]+EBOOK NO\.")
+listing_metadata_pattern = re.compile(r" \[((?:).*)\: ((?:).*)\]")
 
 def extract_year_month(text):
     match = listing_date_pattern.search(text)
@@ -22,14 +21,13 @@ def extract_year_month(text):
         try:
             month_text = match.groupdict().get('month')
             if len(month_text) == 3:
-                month_number = strptime(match.groupdict().get('month'),'%b').tm_mon
+                month_number = strptime(match.groupdict().get('month'), '%b').tm_mon
             else:
-                month_number = strptime(match.groupdict().get('month'),'%B').tm_mon
+                month_number = strptime(match.groupdict().get('month'), '%B').tm_mon
         except:
             month_number = None
-        return year_month(year=int(match.groupdict().get('year')), month = month_number)
-    else:
-        return None
+        return YearMonth(year=int(match.groupdict().get('year')), month=month_number)
+    return None
 
 def get_file_text(link):
     response = get(link)
@@ -49,7 +47,6 @@ def parse_table(text):
 
 def parse_book_info(text):
     metadata = {}
-    current_title = None
     book_id = None
     buffer_lines = []
     for line in text.splitlines():
@@ -75,16 +72,15 @@ def parse_book_info(text):
                 [key, value] = listing_metadata_match.groups()[:2]
                 metadata[key.strip().lower()] = value.strip()
     if title and book_id:
-            title = title.strip()
-            book_data = ({'info_text': combined_text, 'title_line': title, 'book_id':book_id.strip()})
-            title_parts =  title.split(', by')
-            book_data['title'] = title_parts[0]
-            if len(title_parts) > 1:
-                book_data['author'] = title_parts[1].strip()
-            book_data.update(metadata)
-            return book_data
-    else:
-            return None
+        title = title.strip()
+        book_data = ({'info_text': combined_text, 'title_line': title, 'book_id':book_id.strip()})
+        title_parts = title.split(', by')
+        book_data['title'] = title_parts[0]
+        if len(title_parts) > 1:
+            book_data['author'] = title_parts[1].strip()
+        book_data.update(metadata)
+        return book_data
+    return None
 
 def gutenberg_divide_index_into_tables(text):
     halves = re.split(listing_start_pattern, text)
@@ -104,12 +100,15 @@ def arrange_book_text(text):
     return text
 
 def remove_duplicate_spaces(text):
-    return re.sub('(\s){2,}', ' ', text).strip()
+    return re.sub(r'(\s){2,}', ' ', text).strip()
 
 def normalize_text(text):
     return text.replace("\r\n", "\n")
 
+def main():
+    retrieved_text = get_file_text('http://gutenberg.readingroo.ms/GUTINDEX.2018')
+    normalized_text = normalize_text(retrieved_text)
+    print(gutenberg_text_to_json(normalized_text))
+
 if __name__ == '__main__':
-    text = get_file_text('http://gutenberg.readingroo.ms/GUTINDEX.2018')
-    text = normalize_text(text)
-    print(gutenberg_text_to_json(text))
+    main()
